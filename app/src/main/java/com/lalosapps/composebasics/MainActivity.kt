f92347815d5296a9e060c8a4590ae310a96d1d2f
cbarrios <lalobarrios16@yahoo.com>
2022-04-29 12:38:30 -0400

    master: (7) State in Compose

--- app/src/main/java/com/lalosapps/composebasics/MainActivity.kt
+++ app/src/main/java/com/lalosapps/composebasics/MainActivity.kt
@@ -12,6 +12,10 @@ import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,17 +46,23 @@ fun MyApp(names: List<String> = listOf("World", "Compose")) {
 
 @Composable
 fun Greeting(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding = if (expanded) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
-            OutlinedButton(onClick = {}) {
-                Text(text = "Show more")
+            OutlinedButton(onClick = { expanded = !expanded }) {
+                Text(text = if (expanded) "Show Less" else "Show More")
             }
         }
     }
