46347ecddeb89643489804dc2873f26b9fc9fb91
cbarrios <lalobarrios16@yahoo.com>
2022-04-29 10:21:40 -0400

    master: (4) Tweaking the UI

--- app/src/main/java/com/lalosapps/composebasics/MainActivity.kt
+++ app/src/main/java/com/lalosapps/composebasics/MainActivity.kt
@@ -3,13 +3,14 @@ package com.lalosapps.composebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.lalosapps.composebasics.ui.theme.ComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,6 @@ class MainActivity : ComponentActivity() {
             ComposeBasicsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
@@ -31,7 +31,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(
+        color = MaterialTheme.colors.primary
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = Modifier.padding(24.dp)
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
