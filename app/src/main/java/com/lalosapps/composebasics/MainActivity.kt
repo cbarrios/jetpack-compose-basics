43a580e69235ed4c57e7eb007cca98f3f9d70ebb
cbarrios <lalobarrios16@yahoo.com>
2022-05-04 11:09:34 -0400

    master: (13) Finishing touches!

--- app/src/main/java/com/lalosapps/composebasics/MainActivity.kt
+++ app/src/main/java/com/lalosapps/composebasics/MainActivity.kt
@@ -4,13 +4,16 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
@@ -18,6 +21,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,7 +35,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp() {
+private fun MyApp() {
     // rememberSaveable will persist our state on config changes and process death (in scalable apps the view model should handle this)
     var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
     ComposeBasicsTheme {
@@ -47,7 +51,7 @@ fun MyApp() {
 }
 
 @Composable
-fun OnboardingScreen(
+private fun OnboardingScreen(
     onContinuedClicked: () -> Unit
 ) {
     Surface {
@@ -68,7 +72,7 @@ fun OnboardingScreen(
 }
 
 @Composable
-fun Greetings(names: List<String> = List(1000) { "$it" }) {
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
     LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
         item { Text(text = "Header") }
         items(names) { name ->
@@ -78,37 +82,59 @@ fun Greetings(names: List<String> = List(1000) { "$it" }) {
 }
 
 @Composable
-fun Greeting(name: String) {
+private fun Greeting(name: String) {
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+        // Only for Surface. We are using a Card which gives us a nice look without this line of code
+        //.shadow(elevation = 4.dp, clip = true, shape = RoundedCornerShape(8.dp))
+    ) {
+        CardContent(name)
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
     // rememberSaveable can be used here to persist the expanded state since LazyColumn recomposes only the elements that are in the screen
     var expanded by rememberSaveable { mutableStateOf(false) }
-    val extraPadding by animateDpAsState(
-        targetValue = if (expanded) 48.dp else 0.dp,
-        animationSpec = spring( // tween, repeatable are other animations
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring( // tween, repeatable are other animations
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding) // TODO Warning: The spring animation will set padding as negative resulting in a crash
-            ) {
-                Text(text = "Hello,")
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello,")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
                 Text(
-                    text = name,
-                    style = MaterialTheme.typography.h4.copy(
-                        fontWeight = FontWeight.ExtraBold
-                    )
+                    text = ("Composem ipsum color sit lazy, " + "padding theme elit, sed do bouncy. ")
+                        .repeat(4)
                 )
             }
-            OutlinedButton(onClick = { expanded = !expanded }) {
-                Text(text = if (expanded) "Show Less" else "Show More")
-            }
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+            )
         }
     }
 }
@@ -116,14 +142,14 @@ fun Greeting(name: String) {
 @Preview("Light Mode", showBackground = true)
 @Preview("Dark Mode", uiMode = UI_MODE_NIGHT_YES)
 @Composable
-fun MyAppPreview() {
+private fun MyAppPreview() {
     MyApp()
 }
 
 @Preview("Greetings: Light Mode", showBackground = true, widthDp = 320)
 @Preview("Greetings: Dark Mode", uiMode = UI_MODE_NIGHT_YES, widthDp = 320)
 @Composable
-fun GreetingsPreview() {
+private fun GreetingsPreview() {
     ComposeBasicsTheme {
         Greetings()
     }
